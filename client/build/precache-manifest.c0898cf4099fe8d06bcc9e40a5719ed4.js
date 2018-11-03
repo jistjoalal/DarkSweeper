6778845d2d370ff5633195ea4ce2dd2517196fe3
jistjoalal <jistjoalal@gmail.com>
2018-11-03 01:20:31 +0000

    add production build to repo

---
+++ client/build/precache-manifest.c0898cf4099fe8d06bcc9e40a5719ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aacf0cd4b5ec5a6fcd69",
+    "url": "/static/css/main.1da67383.chunk.css"
+  },
+  {
+    "revision": "aacf0cd4b5ec5a6fcd69",
+    "url": "/static/js/main.aacf0cd4.chunk.js"
+  },
+  {
+    "revision": "4b7ea860f0e88eee7fdb",
+    "url": "/static/js/1.4b7ea860.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "932fdead740511783c1dc264f82d5510",
+    "url": "/static/media/logo.932fdead.svg"
+  },
+  {
+    "revision": "54eba06a49ed8e4da7ba111e12479e30",
+    "url": "/index.html"
+  }
+];
