0b60e3b0eeb9822cf91a7041f6b83601efb6e45b
jistjoalal <jistjoalal@gmail.com>
2018-11-03 01:09:22 +0000

    connect client to express app

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.send('hello');
-});
-
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
