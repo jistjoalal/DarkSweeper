d6a6a46c66c74c022af5b9e1aec95f7cf082559d
jistjoalal <jistjoalal@gmail.com>
2019-06-16 11:03:17 -0700

    deploy updated deps

---
+++ client/build/precache-manifest.22166c5650e6b3b7f767924d58ee0a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b4f194032ed320cc21fa021fe0bd67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31bc6f0a41b3711db9a0",
+    "url": "/static/css/main.e64b72b5.chunk.css"
+  },
+  {
+    "revision": "595c64e1b48362394bff",
+    "url": "/static/js/2.9a7efd7d.chunk.js"
+  },
+  {
+    "revision": "31bc6f0a41b3711db9a0",
+    "url": "/static/js/main.5291c629.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eed724adb94e8d981e3a8deb504de902",
+    "url": "/static/media/logo.eed724ad.svg"
+  }
+]);
