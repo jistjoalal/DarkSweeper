5eba368a2f4b4eb62bf46f6bef6191ca60c3a679
jistjoalal <jistjoalal@gmail.com>
2019-10-07 16:42:47 -0700

    update server deps

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const graphqlHTTP = require('express-graphql');
-const mongoose = require('mongoose');
+const path = require("path");
+const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 
 // check env vars
 const { DS_DB_URL, PORT } = process.env;
 // exit if no db url set
 if (!DS_DB_URL) {
-  console.log('DS_DB_URL not set');
+  console.log("DS_DB_URL not set");
   process.exit();
 }
 
 const port = PORT || 3001;
 
 // attempt connect to DB
-mongoose.connect(DS_DB_URL, {useNewUrlParser: true});
-mongoose.connection.once('open', () => {
-  console.log('connected to db');
-})
-
+mongoose.connect(DS_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to db");
+});
 
 // define schema
-const schema = require('./server/schema');
-app.use('/graphql', graphqlHTTP({
-  schema,
-  // disable graphiql in prod
-  graphiql: port === 3001
-}));
+const schema = require("./server/schema");
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    // disable graphiql in prod
+    graphiql: port === 3001
+  })
+);
 
 // serve client build for any route
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*',function (req, res) {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 // run express server
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
